0142cb8e0c60b88aaee3173b6e03d6efd9244ef8
Janek7734 <karakula.jan@gmail.com>
2026-04-29 00:56:24 +0200

    new look

--- janovka/script.js
+++ janovka/script.js
@@ -1,29 +1,8 @@
-const przycisk = document.querySelector(".przycisk-glowny");
-const box = document.querySelector(".box");
+const formularzKontaktowy = document.querySelector(".formularz-kontaktowy");
 
-if (przycisk && box) {
-    przycisk.addEventListener("click", function() {
-        box.classList.toggle("zolty-box");
-
-        if (box.classList.contains("zolty-box")) {
-            przycisk.textContent = "Cofnij kolor";
-        } else {
-            przycisk.textContent = "Kliknij";
-        }
-    });
-}
-
-const naglowek = document.querySelector("#glowny-naglowek");
-const przycisk2 = document.querySelector(".przycisk-drugi");
-
-if (przycisk2 && naglowek) {
-    przycisk2.addEventListener("click", function() {
-        naglowek.classList.toggle("czerwony-naglowek");
-
-        if (naglowek.classList.contains("czerwony-naglowek")) {
-            przycisk2.textContent = "Przywróć kolor";
-        } else {
-            przycisk2.textContent = "Zmień kolor";
-        }
+if (formularzKontaktowy) {
+    formularzKontaktowy.addEventListener("submit", function(event) {
+        event.preventDefault();
+        alert("Dzięki za wiadomość! Na razie formularz jest pokazowy, więc napisz też na maila: karakula.jan@gmail.com");
     });
 }
