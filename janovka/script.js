35c8e9b4aec10b425737492c6dd81f3cf19ffdf3
Janek7734 <karakula.jan@gmail.com>
2026-04-28 23:41:30 +0200

    changes new

--- janovka/script.js
+++ janovka/script.js
@@ -1,25 +1,29 @@
 const przycisk = document.querySelector(".przycisk-glowny");
 const box = document.querySelector(".box");
 
-przycisk.addEventListener("click", function() {
-    box.classList.toggle("zolty-box");
+if (przycisk && box) {
+    przycisk.addEventListener("click", function() {
+        box.classList.toggle("zolty-box");
 
-    if (box.classList.contains("zolty-box")) {
-        przycisk.textContent = "Cofnij kolor";
-    } else {
-        przycisk.textContent = "Kliknij";
-    }
-});
+        if (box.classList.contains("zolty-box")) {
+            przycisk.textContent = "Cofnij kolor";
+        } else {
+            przycisk.textContent = "Kliknij";
+        }
+    });
+}
 
 const naglowek = document.querySelector("#glowny-naglowek");
 const przycisk2 = document.querySelector(".przycisk-drugi");
 
-przycisk2.addEventListener("click", function() {
-    naglowek.classList.toggle("czerwony-naglowek");
+if (przycisk2 && naglowek) {
+    przycisk2.addEventListener("click", function() {
+        naglowek.classList.toggle("czerwony-naglowek");
 
-    if (naglowek.classList.contains("czerwony-naglowek")) {
-        przycisk2.textContent = "Przywróć kolor";
-    } else {
-        przycisk2.textContent = "Zmień kolor";
-    }
-});
+        if (naglowek.classList.contains("czerwony-naglowek")) {
+            przycisk2.textContent = "Przywróć kolor";
+        } else {
+            przycisk2.textContent = "Zmień kolor";
+        }
+    });
+}
