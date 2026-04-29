5ad18cb6ae2fa64620385a33e01d8a00ffabcee7
Janek7734 <karakula.jan@gmail.com>
2026-04-29 15:32:46 +0200

    last changes

--- janovka/script.js
+++
@@ -1,8 +0,0 @@
-const formularzKontaktowy = document.querySelector(".formularz-kontaktowy");
-
-if (formularzKontaktowy) {
-    formularzKontaktowy.addEventListener("submit", function(event) {
-        event.preventDefault();
-        alert("Dzięki za wiadomość! Na razie formularz jest pokazowy, więc napisz też na maila: karakula.jan@gmail.com");
-    });
-}
